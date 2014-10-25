9acfa92b0e011011c5066c3a5b4b6a34bf559c03
k2works <kakimomokuri@gmail.com>
2014-10-25 19:14:34 +0900

    Setup

---
+++ travelphoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  travelphoto
+//
+//  Created by Kakigi Katuyuki on 2014/10/25.
+//  Copyright (c) 2014年 Kakigi Katuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
