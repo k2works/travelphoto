191e5b658fab07b65ebab2587cdd42bccdb305df
k2works <kakimomokuri@gmail.com>
2014-11-23 16:46:21 +0900

    identifierの処理

--- travelphoto/Model/Travel.m
+++ travelphoto/Model/Travel.m
@@ -9,5 +9,12 @@
 @implementation Travel
 
 // Custom logic goes here.
+- (void)awakeFromInsert {
+    [super awakeFromInsert];
+    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+    NSString *uuidStr = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+    CFRelease(uuid);
+    self.identifier = uuidStr;
+}
 
 @end
